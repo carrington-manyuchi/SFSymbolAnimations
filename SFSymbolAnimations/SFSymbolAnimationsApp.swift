2687454d9ed51c4d17ebb0373a35514b9b8d8bfa
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-12-18 07:57:12 +0200

    Initial Commit

---
+++ SFSymbolAnimations/SFSymbolAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFSymbolAnimationsApp.swift
+//  SFSymbolAnimations
+//
+//  Created by Manyuchi, Carrington C on 2024/12/18.
+//
+
+import SwiftUI
+
+@main
+struct SFSymbolAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
